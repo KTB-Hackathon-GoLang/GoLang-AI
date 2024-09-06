b610b7cd2ee8118a1f772d64b98c524ce3f845bf
정은채 <goldchae@jeong-eunchaeui-MacBookPro-2.local>
2024-09-06 19:40:50 +0900

    fix : 프롬프트 업데이트

--- prompts/purify_prompt.py
+++ prompts/purify_prompt.py
@@ -2,6 +2,7 @@ from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTempla
 
 purify_system_message = '''### 지시 ### 
 주어지는 문장을 순화하라. 욕설이 있으면 제외하라. 격한 표현이 있으면 돌려 표현하라. 조롱조가 있으면 표현을 긍정적으로 바꿔 표현하라. 단, 담고 있는 의미는 누락되어서는 안 된다. 
+순화할 표현이 아무것도 없다면 그대로 출력해도 된다.
 ### 실행 순서 ###
 1. 비속어를 순화한 문장을 3가지 만들어라.
 이때 먼저 상대방의 감정에 공감, 나의 감정 전달, 나의 의도를 전달하면서 순화하면 좋다. 
--- prompts/summary_prompt.py
+++ prompts/summary_prompt.py
@@ -3,6 +3,7 @@ from langchain_core.prompts import ChatPromptTemplate, SystemMessagePromptTempla
 summary_system_message = '''### 지시 ###
 주어지는 문장은 두서없이 작성한 문장이다. 내용을 요약하고 정리하라. 단, 원본 문장의 말투는 변경하지 마시오.
 총 2가지 다른 방식으로 요약하라.
+정리할 내용이 아무것도 없다면 그대로 출력해도 된다.
 
 ### 출력 형식 ###
 json 형식으로 출력하되 아래 형식으로 출력하라.
