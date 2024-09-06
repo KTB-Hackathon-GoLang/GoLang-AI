3e235598313dd1107ff2eeb5c151c4f38f1a5af9
taejin1221 <taejin7824@gmail.com>
2024-09-06 19:33:28 +0900

    Feat: GPT-4o-mini 사용

--- LLM/llm.py
+++ LLM/llm.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI
 import secret
 
 AI_model = ChatOpenAI(
-    model="gpt-4",
+    model="gpt-4o-mini",
     temperature=0.75, 
     max_tokens = 1024,
     openai_api_key = secret.openai_api_key
