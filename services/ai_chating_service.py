4a694ba92e02787f87cb998817547d99910234a6
JungEunChae <104445068+Goldchae@users.noreply.github.com>
2024-09-07 01:51:34 +0900

    ✨ Feat : AI 1인용 채팅 기능 (#28)
    * Fix: Error 수정 (#20)
    
    * fix: 컨텍스트 돌아가게 수정
    
    * fix : 프롬프트 업데이트
    
    * feat: rag
    
    * fix
    
    * fix
    
    * feat: rag 수정
    
    * fix: EXPLAIN_SITUATION cnrk
    
    * Delete golang/share/man/man1/isympy.1
    
    * Remove share/man directory from Git
    
    * Remove share/man directory from Git
    
    * feat: ai 채팅
    
    ---------
    
    Co-authored-by: wrathlion <taejin7824@gmail.com>
    Co-authored-by: 정은채 <goldchae@jeong-eunchaeui-MacBookPro-2.local>

---
+++ services/ai_chating_service.py
@@ -0,0 +1,25 @@
+from langchain_core.output_parsers import StrOutputParser
+from aiModel import llm
+from prompts import ai_chat_prompt
+from services import context
+from services import rag_service
+
+
+def ai_chating(user_id, chating_room_id, relation, explain_situation, message, file_path=None): 
+    history = context.get_user_memory(user_id)
+    
+    if file_path:
+        retriever = rag_service.make_rag_file(file_path,chating_room_id)
+        related_documents = retriever.get_relevant_documents(message)
+    else:
+        related_documents = "none"
+
+    formatted_messages = ai_chat_prompt.make_ai_chating_prompt(relation,  explain_situation,message, history, related_documents)
+    response = llm.AI_model.invoke(formatted_messages)
+        
+    parser = StrOutputParser()
+    parsed_output = parser.parse(response)
+
+    context.add_message(user_id, message, parsed_output.content)
+    
+    return parsed_output.content
