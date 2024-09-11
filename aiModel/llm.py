8227b9c846c299b60dce86aa06942d10128dc162
taejin1221 <taejin7824@gmail.com>
2024-09-11 20:22:39 +0900

    Docs: README.md 추가 및 Style 일치화

--- aiModel/llm.py
+++ aiModel/llm.py
@@ -5,6 +5,6 @@ import secret
 AI_model = ChatOpenAI(
     model="gpt-4o-mini",
     temperature=0.75, 
-    max_tokens = 1024,
-    openai_api_key = secret.openai_api_key
+    max_tokens=1024,
+    openai_api_key=secret.openai_api_key
 )
