36655a0df02053320e2b0ee9761fcfc4bc0777c4
taejin1221 <taejin7824@gmail.com>
2024-09-11 19:36:55 +0900

    Fix: chatroom id 바로 찾는 오류 수정

--- repository/chat_file_repository.py
+++ repository/chat_file_repository.py
@@ -3,8 +3,9 @@ from repository.database import request_connect_db
 class ChatFileRepository:
     db = request_connect_db('test')['DB']
 
-    def find_by_chatroom_id(self, chatroom_id: str):
-        response = self.db.execute(f"SELECT filename FROM chat_file WHERE chatroom_id = {chatroom_id}")
+    def find_by_chatroom_id(self, chatroom_uuid: str):
+        chatroom_id = self.db.execute(f'SELECT chatroom_id FROM chatroom WHERE chatroom_uuid = "{chatroom_uuid}"')[0]['chatroom_id']
+        response = self.db.execute(f'SELECT filename FROM chat_file WHERE chatroom_id = "{chatroom_id}"')
 
         if type(response) == list:
             return response[0]['filename']
